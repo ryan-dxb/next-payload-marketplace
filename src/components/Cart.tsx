37068043145731d8cf395d8c4658cdd1acdf89b7
Ryan Pereira <ryan.dxb92@gmail.com>
2023-11-21 15:52:16 +0400

    Added Payload admin cms backend

---
+++ src/components/Cart.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import React, { FC, useEffect, useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { ShoppingCart } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import Image from "next/image";
+
+interface CartProps {}
+
+const Cart: FC<CartProps> = () => {
+  const itemCount = 0;
+
+  const fee = 10;
+
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return (
+    <Sheet>
+      <SheetTrigger className="flex items-center p-2 -m-2 group">
+        <ShoppingCart className="flex-shrink-0 w-6 h-6 text-gray-400 group-hover:text-gray-500" />
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          {isMounted ? itemCount : 0}
+        </span>
+      </SheetTrigger>
+
+      <SheetContent className="flex flex-col w-full pr-0 sm:max-w-lg">
+        <SheetHeader className="space-y-2.5 pr-6">
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
+        </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex flex-col w-full pr-6">
+              {/* CART ITEMS */}
+              cart Items
+            </div>
+
+            <div className="pr-6 space-y-4">
+              <Separator />
+
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(2000 + fee)}</span>
+                </div>
+              </div>
+
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({
+                      className: "w-full",
+                    })}
+                  >
+                    {" "}
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div className="flex flex-col items-center justify-center h-full space-y-1">
+            <div
+              aria-hidden="true"
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+            >
+              <Image
+                src="/hippo-empty-cart.png"
+                fill
+                alt="empty shopping cart hippo"
+              />
+            </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link
+                href="/products"
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-foreground",
+                })}
+              >
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
+          </div>
+        )}
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default Cart;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,14 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
+  const user = null;
+
   return (
     <div className="sticky inset-x-0 top-0 z-50 h-16 bg-white">
       <header className="relative bg-white">
@@ -23,6 +27,52 @@ const Navbar: FC<NavbarProps> = () => {
 
               <div className="z-50 hidden lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+
+              <div className="flex items-center ml-auto">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href="/login"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span className="w-px h-6 bg-gray-200" aria-hidden="true" />
+                  )}
+
+                  {user ? null : ( // <UserAccountNav user={user} />
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({
+                        variant: "ghost",
+                      })}
+                    >
+                      Create account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span className="w-px h-6 bg-gray-200" aria-hidden="true" />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="w-px h-6 bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
+
+                  <div className="flow-root ml-4 lg:ml-6">
+                    {" "}
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
